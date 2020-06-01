4dc48917bc091f3e831afeb6a7e51a7a3b6839d4
SaiAutomation99 <saiprasanth.qa@gmail.com>
2020-06-01 12:43:47 +0530

    added expectedexceptions

---
+++ src/test/java/ExpectedException/ExpectedExceptions.java
@@ -0,0 +1,17 @@
+package ExpectedException;
+
+import org.testng.annotations.Test;
+
+public class ExpectedExceptions {
+	
+	
+	
+	@Test(expectedExceptions = ArithmeticException.class)
+	public void m1() {
+		
+		System.out.println("m1 method");
+		throw new ArithmeticException();
+		
+	}
+
+}
