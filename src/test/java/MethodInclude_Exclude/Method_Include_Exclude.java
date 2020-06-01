50aa247fefa201eb8110b031c963e0dc58d2ebd8
SaiAutomation99 <saiprasanth.qa@gmail.com>
2020-06-01 12:21:54 +0530

    added methods tag in xml

---
+++ src/test/java/MethodInclude_Exclude/Method_Include_Exclude.java
@@ -0,0 +1,22 @@
+package MethodInclude_Exclude;
+
+import org.testng.annotations.Test;
+
+public class Method_Include_Exclude {
+	
+	
+	@Test
+	public void m1() {
+		
+		
+		System.out.println("m1 method");
+	}
+	
+	
+	@Test
+	public void m2() {
+		
+		System.out.println("m2 method");
+	}
+
+}
