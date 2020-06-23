59230d1f05d59a794bb9c0fe1e30bb929fad27c1
SaiAutomation99 <saiprasanth.qa@gmail.com>
2020-06-23 18:25:09 +0530

    added

---
+++ src/test/java/FashionWebSite/FashionWebsite.java
@@ -0,0 +1,32 @@
+package FashionWebSite;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class FashionWebsite {
+	
+	
+	public WebDriver driver;
+	@Test
+	public void fashion() {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://automationpractice.com/index.php");
+		WebDriverWait wait=new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.xpath("(//li[@class='homeslider-container'])[3]"))));
+		driver.findElement(By.xpath("(//li[@class='homeslider-container'])[3]")).click();
+		driver.quit();
+		
+	}
+
+}
